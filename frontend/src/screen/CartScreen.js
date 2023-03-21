839404e32892e66a676ac10e0fa779ee68c10982
Santiago Sevilla <santisevilla.9@gmail.com>
2023-03-21 20:54:29 -0300

    adsd

--- frontend/src/screen/CartScreen.js
+++ frontend/src/screen/CartScreen.js
@@ -7,14 +7,36 @@ import MessageBox from "./components/MessageBox";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import axios from "axios";
 
 function CartScreen() {
+  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.stock < quantity) {
+      window.alert("Product is out of stock");
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+
+  const removeItemHandler = (item) => {
+    ctxDispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+
+  const checkoutHandler = () => {
+    navigate("signin?redirect=/shipping");
+  };
 
   return (
     <div>
@@ -42,12 +64,21 @@ function CartScreen() {
                       <Link to={`/product/${item.slug}`}>{item.name}</Link>
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" disabled={item.quantity === 1}>
+                      <Button
+                        variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
+                        disabled={item.quantity === 1}
+                      >
                         <i className="fas fa-minus-circle"></i>
                       </Button>{" "}
                       <span>{item.quantity}</span>{" "}
                       <Button
                         variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
                         disabled={item.quantity === item.stock}
                       >
                         <i className="fas fa-plus-circle"></i>
@@ -55,7 +86,10 @@ function CartScreen() {
                     </Col>
                     <Col md={3}>$ {item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button
+                        onClick={() => removeItemHandler(item)}
+                        variant="light"
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -71,20 +105,22 @@ function CartScreen() {
               <ListGroup variant="flush">
                 <ListGroupItem>
                   <h3>
-                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{" "}items) : 
-                    ${" "} {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{" "}
+                    items) : ${" "}
+                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
                   </h3>
                 </ListGroupItem>
                 <ListGroupItem>
-                    <div className="d-grid">
-                        <Button
-                            type="button"
-                            variant="primary"
-                            disabled={cartItems.length === 0}
-                        >
-                            Proceed to Checkout
-                        </Button>
-                    </div>
+                  <div className="d-grid">
+                    <Button
+                      type="button"
+                      variant="primary"
+                      onClick={checkoutHandler}
+                      disabled={cartItems.length === 0}
+                    >
+                      Proceed to Checkout
+                    </Button>
+                  </div>
                 </ListGroupItem>
               </ListGroup>
             </Card.Body>
--- frontend/src/screen/components/Product.js
+++ frontend/src/screen/components/Product.js
@@ -1,10 +1,32 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import Rating from "./Rating.js"
+import Rating from "./Rating.js";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../../Store.js";
 
 function Product(props) {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.stock < quantity) {
+      window.alert("Product is out of stock");
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -14,9 +36,16 @@ function Product(props) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add to cart</Button>
+        {product.stock === 0 ? (
+          <Button variant="light" disabled>
+            {" "}
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
